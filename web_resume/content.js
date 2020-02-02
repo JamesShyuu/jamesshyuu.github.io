7fa8815889d5133c646e9664eadb4b35729ca5dc
james.hsu <james.hsu@finsage.com.tw>
2020-02-03 00:15:20 +0800

    履歷修改

---
+++ web_resume/content.js
@@ -0,0 +1,84 @@
+// 參與專案 愛物聯
+var iiot = {
+    projModal_title: '愛物聯股份有限公',
+    projModal_img1: 'images/web_design_iiot1.png',
+    projModal_img2: 'images/web_design_iiot2.jpg',
+    projModal_label1: 'wifi機與後台系統整合服務',
+    projModal_duty1: '負責部分：前端工程、UIUX',
+    projModal_skill1: '使用技術：Angular JS, Angular, Gulp, TypeScript, Bootstrap, HTML, CSS, SCSS',
+    projModal_descibe1:'說明：產品為後台管理系統。主要任務為開發新功能、UI調整、畫面美化。',
+    projModal_period1: '參與時間：2017.11~2018.02',
+
+}
+
+// 參與專案 全球人壽
+var insurance = {
+    projModal_title: '全球人壽',
+    projModal_img1: 'images/web_design_gb1.jpg',
+    projModal_img2: 'images/web_design_gb2.jpg',
+    projModal_label1: '全球人壽行動投保(mPos)',
+    projModal_duty1: '負責部分：前端工程',
+    projModal_skill1: '使用技術：HTML, CSS, Angular JS, Bootstrap, RequireJS',
+    projModal_descibe1: '說明：此產品為ios app，使用UIWebView載入網頁內容。使從業人員能攜帶ipad外出與客戶討論商品，並且能在離線的狀態下進行投保的工作。',
+    projModal_period1: '參與時間：2017.01~2018.03',
+    projModal_label2: '全球人壽業務人員管理系統',
+    projModal_duty2: '負責部分：前端工程',
+    projModal_skill2: '使用技術：Angular JS, JQuery, Bootstrap, RequireJS, AJAX, HTML, CSS',
+    projModal_descibe2: '說明：業務人員後台管理系統，並會依登入者的身份權限有不同功能。',
+    projModal_period2: '參與時間：2017.01~2018.03'
+}
+
+// 參與專案 中華郵政
+var post = {
+    projModal_title: '中華郵政',
+    projModal_img1: 'images/web_design_pos1.png',
+    projModal_img2: 'images/web_design_pos2.png',
+    projModal_label1: '網路郵局(英文版)',
+    projModal_duty1: '負責部分：前端工程、UIUX',
+    projModal_skill1: '使用技術：Angular JS, Java, JQuery, JQuery UI, HTML, CSS',
+    projModal_descibe1: '說明：本系統為對外系統，給使用者處理郵政業務或儲蓄業務。在本專案中主要任務為UI調整、元件錯誤修正、交易撰寫',
+    projModal_period1: '參與時間：2019.10~2020.01',
+    porjModal_url: 'https://freeipost.post.gov.tw/pstEN/home.html'
+}
+
+// 參與專案 台灣銀行
+var bis = {
+    projModal_title: '台灣銀行',
+    projModal_img1: 'images/web_design_bis1.png',
+    projModal_img2: 'images/web_design_bis1.png',
+    projModal_label1: '分行端末系統',
+    projModal_duty1: '負責部分：前端工程',
+    projModal_skill1: '使用技術：Angular, Java, JQuery, TypeScript, Bootstrap, HTML, CSS',
+    projModal_descibe1: '說明：本系統為對內系統，給銀行內從業人處理各項銀行業務之系統。在本專案中主要任務為前端平台，共用元件與方法撰寫，交易撰寫。',
+    projModal_period1: '參與時間：2018.08~2019.08'
+}
+
+
+// 專案設計 旅歷
+var travel = {
+    projDesign_title: '旅歷',
+    projDesign_label1: '旅遊搜尋規劃平台',
+    projDesign_img: 'images/travel.jpg',
+    projDesign_duty1: '負責部分：前端工程、UI',
+    projDesign_skill1: '使用技術：JavaScript, JQuery, Bootstrap, Font Awesome, HTML, CSS',
+    projDesign_skill2: '',
+    projDesign_skill3: '',
+    projDesign_descibe1: '說明：此專案為北科大結訓作品(＊目前只呈現前端部分)，設計給旅遊愛好者一個快速搜尋和規劃行程的平台，並且有會員系統與旅遊文章',
+    projDesign_period1: '開發時程：2016.08~2016.11',
+    projDesign_url1: 'https://jamesshyuu.github.io/james_work/home_page.html'
+}
+
+
+// 專案設計 要吃啥
+var lunchBox = {
+    projDesign_title: '要吃啥',
+    projDesign_label1: '網頁訂餐平台',
+    projDesign_img: 'images/lunchBox_twoDevice.jpg',
+    projDesign_duty1: '負責部分：前端工程、後端工程、資料庫、UI',
+    projDesign_skill1: '使用技術：前端：Angular, JQuery, Bootstrap, HTML, CSS',
+    projDesign_skill2: '        後端：Node JS',
+    projDesign_skill3: '        資料庫：Firebase',
+    projDesign_descibe1: '說明：此專案設計給每位不想日曬、淋雨的上班族，快速訂餐與外送服務的平台。此專案有會員系統與購物車，讓使用者能快速下單、查詢訂單、修改訂單，並且擁有優惠卷與紅利機制，讓你每次的訂購都能多省一點。另外還有後台管理系統，讓管理者能掌握每天的訂單。 (測試帳號：testlunchbox@hotmail.com, 密碼：123456)',
+    projDesign_period1: '開發時程：2019.02~2020.01',
+    projDesign_url1: 'https://lunch-box-app.appspot.com/'
+}
--- web_resume/js/controller.js
+++ web_resume/js/controller.js
@@ -1,18 +1,21 @@
 
 //my profile 北科大履歷's var
-var travelPic1 =  document.getElementById('travelPic1');
+//var travelPic1 =  document.getElementById('travelPic1');
 /* var webDesign_travel = document.getElementById('webDesign_travel');
 var webDesign_travelContent = document.getElementById('webDesign_travelContent'); */
 // var travelPic2 =  document.getElementById('travelPic2');
 // var travelPic3 =  document.getElementById('travelPic3');
 // travelPic2.style.opacity = 0;
 // travelPic3.style.opacity = 0;
-var slides1 = document.getElementsByClassName("mySlides_1");//北科大
+//var slides1 = document.getElementsByClassName("mySlides_1");//北科大
 var dots1 = document.getElementsByClassName("myProfile_slideShow_dot_1");//北科大
 var slides2 = document.getElementsByClassName("mySlides_2");//全球人壽
 var dots2 = document.getElementsByClassName("myProfile_slideShow_dot_2");//全球人壽
 var slides3 = document.getElementsByClassName("mySlides_3");//iiot
 var dots3 = document.getElementsByClassName("myProfile_slideShow_dot_3");//iiot
+
+var slidePublic = document.getElementsByClassName("slide_public");//共用
+var dotsPublic = document.getElementsByClassName("myProfile_slideShow_dot_public");//共用
 
 
 /*
@@ -22,10 +25,12 @@ var dots3 = document.getElementsByClassName("myProfile_slideShow_dot_3");//iiot
 var slideIndex_1 = 1;
 var slideIndex_2 = 1;
 var slideIndex_3 = 1;
+var slideIndex_public = 1;
 /* showSlides(slideIndex); */
 showSlides(slideIndex_1, 1);
 showSlides(slideIndex_2, 2);
 showSlides(slideIndex_3, 3);
+showSlides(slideIndex_public, 4);
 
 function plusSlides(n, num) {
     if(num == 1){
@@ -34,6 +39,8 @@ function plusSlides(n, num) {
         showSlides(slideIndex_2 += n, num);
     }else if(num == 3){
         showSlides(slideIndex_3 += n, num);
+    } else if(num == 4){
+        showSlides(slideIndex_public += n, num);
     }
   /* showSlides(slideIndex += n, num); */
 }
@@ -45,6 +52,8 @@ function currentSlide(n, num) {
         showSlides(slideIndex_2 = n, num);
     }else if(num == 3){
         showSlides(slideIndex_3 = n, num);
+    } else if (num == 4) {
+        showSlides(slideIndex_public = n, num);
     }
   /* showSlides(slideIndex = n, num); */
 }
@@ -54,59 +63,91 @@ function showSlides(n, num) {
   var sildeNum1;
   var sildeNum2;
   var sildeNum3;
-  /* var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("myProfile_slideShow_dot"); */
- /*  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" myProfile_slideShow_active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " myProfile_slideShow_active"; */
-
-  if(num == 1){
-    sildeNum1 = n;
-    if (n > slides1.length) {slideIndex_1 = 1}    
-    if (n < 1) {slideIndex_1 = slides1.length}
-    for (i = 0; i < slides1.length; i++) {
-        slides1[i].style.display = "none";  
-    }
-    for (i = 0; i < dots1.length; i++) {
-        dots1[i].className = dots1[i].className.replace(" myProfile_slideShow_active_1", "");
-    }
-    slides1[slideIndex_1-1].style.display = "block";  
-    dots1[slideIndex_1-1].className += " myProfile_slideShow_active_1";
-
-  }else if(num == 2){
-    sildeNum2 = n;
-    if (n > slides2.length) {slideIndex_2 = 1}    
-    if (n < 1) {slideIndex_2 = slides2.length}
-    for (i = 0; i < slides2.length; i++) {
-        slides2[i].style.display = "none";  
-    }
-    for (i = 0; i < dots2.length; i++) {
-        dots2[i].className = dots2[i].className.replace(" myProfile_slideShow_active_2", "");
-    }
-    slides2[slideIndex_2-1].style.display = "block";  
-    dots2[slideIndex_2-1].className += " myProfile_slideShow_active_2";
-
-  }else if(num == 3){
-    sildeNum3 = n;
-    if (n > slides3.length) {slideIndex_3 = 1}    
-    if (n < 1) {slideIndex_3 = slides3.length}
-    for (i = 0; i < slides3.length; i++) {
-        slides3[i].style.display = "none";  
-    }
-    for (i = 0; i < dots3.length; i++) {
-        dots3[i].className = dots3[i].className.replace(" myProfile_slideShow_active_3", "");
-    }
-    slides3[slideIndex_3-1].style.display = "block";  
-    dots3[slideIndex_3-1].className += " myProfile_slideShow_active_3";
+  var sildeNum_public;
+  
+   if(num == 4){ // 共用 ＊未來只有這個
+      sildeNum_public = n;
+      if (n > slidePublic.length) { slideIndex_public = 1 }
+      if (n < 1) { slideIndex_public = slidePublic.length }
+      for (i = 0; i < slidePublic.length; i++) {
+          slidePublic[i].style.display = "none";
+      }
+      for (i = 0; i < dotsPublic.length; i++) {
+          dotsPublic[i].className = dotsPublic[i].className.replace(" myProfile_slideShow_active_public", "");
+      }
+      slidePublic[slideIndex_public - 1].style.display = "block";
+      dotsPublic[slideIndex_public - 1].className += " myProfile_slideShow_active_public";
   }
 }
 /*
     slide show end Jul 12 18 
 */
+
+
+function projModalShow(key) {
+    if (key == 'iiot') {
+        document.getElementById('projModal_title').innerHTML = iiot.projModal_title;
+        document.getElementById('projModal_img1').src = iiot.projModal_img1;
+        document.getElementById('projModal_img2').src = iiot.projModal_img2;
+        document.getElementById('projModal_label1').innerHTML = iiot.projModal_label1;
+        document.getElementById('projModal_duty1').innerHTML = iiot.projModal_duty1;
+        document.getElementById('projModal_skill1').innerHTML = iiot.projModal_skill1;
+        document.getElementById('projModal_descibe1').innerHTML = iiot.projModal_descibe1;
+        document.getElementById('projModal_period1').innerHTML = iiot.projModal_period1;
+    } else if (key == 'post') {
+        document.getElementById('projModal_title').innerHTML = post.projModal_title;
+        document.getElementById('projModal_img1').src = post.projModal_img1;
+        document.getElementById('projModal_img2').src = post.projModal_img2;
+        document.getElementById('projModal_label1').innerHTML = post.projModal_label1;
+        document.getElementById('projModal_duty1').innerHTML = post.projModal_duty1;
+        document.getElementById('projModal_skill1').innerHTML = post.projModal_skill1;
+        document.getElementById('projModal_descibe1').innerHTML = post.projModal_descibe1;
+        document.getElementById('projModal_period1').innerHTML = post.projModal_period1;
+    } else if (key == 'insurance') {
+        document.getElementById('projModal_title').innerHTML = insurance.projModal_title;
+        document.getElementById('projModal_img1').src = insurance.projModal_img1;
+        document.getElementById('projModal_img2').src = insurance.projModal_img2;
+        document.getElementById('projModal_label1').innerHTML = insurance.projModal_label1;
+        document.getElementById('projModal_duty1').innerHTML = insurance.projModal_duty1;
+        document.getElementById('projModal_skill1').innerHTML = insurance.projModal_skill1;
+        document.getElementById('projModal_descibe1').innerHTML = insurance.projModal_descibe1;
+        document.getElementById('projModal_label2').innerHTML = insurance.projModal_label2;
+        document.getElementById('projModal_duty2').innerHTML = insurance.projModal_duty2;
+        document.getElementById('projModal_skill2').innerHTML = insurance.projModal_skill2;
+        document.getElementById('projModal_descibe2').innerHTML = insurance.projModal_descibe2;
+        document.getElementById('projModal_period2').innerHTML = insurance.projModal_period2;
+    } else if (key == 'tBank') {
+        document.getElementById('projModal_title').innerHTML = bis.projModal_title;
+        document.getElementById('projModal_img1').src = bis.projModal_img1;
+        document.getElementById('projModal_img2').src = bis.projModal_img2;
+        document.getElementById('projModal_label1').innerHTML = bis.projModal_label1;
+        document.getElementById('projModal_duty1').innerHTML = bis.projModal_duty1;
+        document.getElementById('projModal_skill1').innerHTML = bis.projModal_skill1;
+        document.getElementById('projModal_descibe1').innerHTML = bis.projModal_descibe1;
+        document.getElementById('projModal_period1').innerHTML = bis.projModal_period1;
+    }
+}
+
+function projDesignModalShow(key){
+    if(key == 'lunchBox'){
+        document.getElementById('projDesign_title').innerHTML = lunchBox.projDesign_title;
+        document.getElementById('projDesign_img').src = lunchBox.projDesign_img;
+        document.getElementById('projDesign_label1').innerHTML = lunchBox.projDesign_label1;
+        document.getElementById('projDesign_duty1').innerHTML = lunchBox.projDesign_duty1;
+        document.getElementById('projDesign_skill1').innerHTML = lunchBox.projDesign_skill1;
+        document.getElementById('projDesign_skill2').innerHTML = lunchBox.projDesign_skill2;
+        document.getElementById('projDesign_skill3').innerHTML = lunchBox.projDesign_skill3;
+        document.getElementById('projDesign_descibe1').innerHTML = lunchBox.projDesign_descibe1;
+        document.getElementById('projDesign_period1').innerHTML = lunchBox.projDesign_period1;
+        document.getElementById('projDesign_url1').href = lunchBox.projDesign_url1;
+    } else if(key == 'travel'){
+        document.getElementById('projDesign_title').innerHTML = travel.projDesign_title;
+        document.getElementById('projDesign_img').src = travel.projDesign_img;
+        document.getElementById('projDesign_label1').innerHTML = travel.projDesign_label1;
+        document.getElementById('projDesign_duty1').innerHTML = travel.projDesign_duty1;
+        document.getElementById('projDesign_skill1').innerHTML = travel.projDesign_skill1;
+        document.getElementById('projDesign_descibe1').innerHTML = travel.projDesign_descibe1;
+        document.getElementById('projDesign_period1').innerHTML = travel.projDesign_period1;
+        document.getElementById('projDesign_url1').href = travel.projDesign_url1;
+    }
+}

fbc7ce95acfbe72d8c32c6ff7e744a5174f28f59
JamesShyuu <jamesshyuu@gmail.com>
2021-01-31 23:08:28 +0800

    廢棄程式碼與套件移除1

--- web_resume/content.js
+++ web_resume/content.js
@@ -1,57 +1,57 @@
 // 參與專案 愛物聯
-var iiot = {
-    projModal_title: '愛物聯股份有限公',
-    projModal_img1: 'images/web_design_iiot1.png',
-    projModal_img2: 'images/web_design_iiot2.jpg',
-    projModal_label1: 'wifi機與後台系統整合服務',
-    projModal_duty1: '負責部分：前端工程、UIUX',
-    projModal_skill1: '使用技術：Angular JS, Angular, Gulp, TypeScript, Bootstrap, HTML, CSS, SCSS',
-    projModal_descibe1:'說明：產品為後台管理系統。主要任務為開發新功能、UI調整、畫面美化。',
-    projModal_period1: '參與時間：2017.11~2018.02',
+// var iiot = {
+//     projModal_title: '愛物聯股份有限公',
+//     projModal_img1: 'images/web_design_iiot1.png',
+//     projModal_img2: 'images/web_design_iiot2.jpg',
+//     projModal_label1: 'wifi機與後台系統整合服務',
+//     projModal_duty1: '負責部分：前端工程、UIUX',
+//     projModal_skill1: '使用技術：Angular JS, Angular, Gulp, TypeScript, Bootstrap, HTML, CSS, SCSS',
+//     projModal_descibe1:'說明：產品為後台管理系統。主要任務為開發新功能、UI調整、畫面美化。',
+//     projModal_period1: '參與時間：2017.11~2018.02',
 
-}
+// }
 
 // 參與專案 全球人壽
-var insurance = {
-    projModal_title: '全球人壽',
-    projModal_img1: 'images/web_design_gb1.jpg',
-    projModal_img2: 'images/web_design_gb1.jpg',
-    projModal_label1: '全球人壽行動投保(mPos)',
-    projModal_duty1: '負責部分：前端工程',
-    projModal_skill1: '使用技術：HTML, CSS, Angular JS, Bootstrap, RequireJS',
-    projModal_descibe1: '說明：此產品為ios app，使用UIWebView載入網頁內容。使從業人員能攜帶ipad外出與客戶討論商品，並且能在離線的狀態下進行投保的工作。',
-    projModal_period1: '參與時間：2017.01~2018.03',
-    projModal_label2: '全球人壽業務人員管理系統',
-    projModal_duty2: '負責部分：前端工程',
-    projModal_skill2: '使用技術：Angular JS, JQuery, Bootstrap, RequireJS, AJAX, HTML, CSS',
-    projModal_descibe2: '說明：業務人員後台管理系統，並會依登入者的身份權限有不同功能。',
-    projModal_period2: '參與時間：2017.01~2018.03'
-}
+// var insurance = {
+//     projModal_title: '全球人壽',
+//     projModal_img1: 'images/web_design_gb1.jpg',
+//     projModal_img2: 'images/web_design_gb1.jpg',
+//     projModal_label1: '全球人壽行動投保(mPos)',
+//     projModal_duty1: '負責部分：前端工程',
+//     projModal_skill1: '使用技術：HTML, CSS, Angular JS, Bootstrap, RequireJS',
+//     projModal_descibe1: '說明：此產品為ios app，使用UIWebView載入網頁內容。使從業人員能攜帶ipad外出與客戶討論商品，並且能在離線的狀態下進行投保的工作。',
+//     projModal_period1: '參與時間：2017.01~2018.03',
+//     projModal_label2: '全球人壽業務人員管理系統',
+//     projModal_duty2: '負責部分：前端工程',
+//     projModal_skill2: '使用技術：Angular JS, JQuery, Bootstrap, RequireJS, AJAX, HTML, CSS',
+//     projModal_descibe2: '說明：業務人員後台管理系統，並會依登入者的身份權限有不同功能。',
+//     projModal_period2: '參與時間：2017.01~2018.03'
+// }
 
 // 參與專案 中華郵政
-var post = {
-    projModal_title: '中華郵政',
-    projModal_img1: 'images/web_design_pos1.png',
-    projModal_img2: 'images/web_design_pos2.png',
-    projModal_label1: '網路郵局(英文版)',
-    projModal_duty1: '負責部分：前端工程、UIUX',
-    projModal_skill1: '使用技術：Angular JS, Java, JQuery, JQuery UI, HTML, CSS',
-    projModal_descibe1: '說明：本系統為對外系統，給使用者處理郵政業務或儲蓄業務。在本專案中主要任務為UI調整、元件錯誤修正、交易撰寫',
-    projModal_period1: '參與時間：2019.10~2020.01',
-    porjModal_url: 'https://freeipost.post.gov.tw/pstEN/home.html'
-}
+// var post = {
+//     projModal_title: '中華郵政',
+//     projModal_img1: 'images/web_design_pos1.png',
+//     projModal_img2: 'images/web_design_pos2.png',
+//     projModal_label1: '網路郵局(英文版)',
+//     projModal_duty1: '負責部分：前端工程、UIUX',
+//     projModal_skill1: '使用技術：Angular JS, Java, JQuery, JQuery UI, HTML, CSS',
+//     projModal_descibe1: '說明：本系統為對外系統，給使用者處理郵政業務或儲蓄業務。在本專案中主要任務為UI調整、元件錯誤修正、交易撰寫',
+//     projModal_period1: '參與時間：2019.10~2020.01',
+//     porjModal_url: 'https://freeipost.post.gov.tw/pstEN/home.html'
+// }
 
 // 參與專案 台灣銀行
-var bis = {
-    projModal_title: '台灣銀行',
-    projModal_img1: 'images/web_design_bis1.png',
-    projModal_img2: 'images/web_design_bis1.png',
-    projModal_label1: '分行端末系統',
-    projModal_duty1: '負責部分：前端工程',
-    projModal_skill1: '使用技術：Angular, Java, JQuery, TypeScript, Bootstrap, HTML, CSS',
-    projModal_descibe1: '說明：本系統為對內系統，給銀行內從業人處理各項銀行業務之系統。在本專案中主要任務為前端平台，共用元件與方法撰寫，交易撰寫。',
-    projModal_period1: '參與時間：2018.08~2019.08'
-}
+// var bis = {
+//     projModal_title: '台灣銀行',
+//     projModal_img1: 'images/web_design_bis1.png',
+//     projModal_img2: 'images/web_design_bis1.png',
+//     projModal_label1: '分行端末系統',
+//     projModal_duty1: '負責部分：前端工程',
+//     projModal_skill1: '使用技術：Angular, Java, JQuery, TypeScript, Bootstrap, HTML, CSS',
+//     projModal_descibe1: '說明：本系統為對內系統，給銀行內從業人處理各項銀行業務之系統。在本專案中主要任務為前端平台，共用元件與方法撰寫，交易撰寫。',
+//     projModal_period1: '參與時間：2018.08~2019.08'
+// }
 
 
 // 專案設計 旅歷
